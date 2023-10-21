041f9241f5910f4c96facc50e193b393ed2d1de6
Thorfin <m.a.belkouri@gmail.com>
2023-10-22 00:02:25 +0100

    fixing_the_configuration

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
   //mongoURI: "mongodb://spectre:spectre007@ds253537.mlab.com:53537/elearning", //apurva's
-  mongoURI: 'mongodb+srv://admin:admin@cluster0-xb8vd.gcp.mongodb.net/E-learning?retryWrites=true&w=majority',
+  mongoURI: "mongodb+srv://mightguy911:mightguy911@cluster0.caeyxcu.mongodb.net/Cluster0", //apurva's',
   secretOrKey: "secret"
 };
